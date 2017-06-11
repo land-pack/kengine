95455151eed89221705b8d71e95099a9a63d5329
landpack <ada.frank.ak@gmail.com>
2017-06-11 15:49:06 +0800

    [test] dispatcher.lua | pass

--- demo/dispatcher.lua
+++ demo/dispatcher.lua
@@ -1,53 +1,36 @@
---[[
-    API disabled in the context of set_by_lua*? 
-    https://github.com/openresty/lua-nginx-module/issues/275 
-]] --
+--[[ API disabled in the context of set_by_lua*? 
+    https://github.com/openresty/lua-nginx-module/issues/275 ]] --
 
 local var = ngx.var
---local devicedb = ngx.shared.devicedb
-
 local redis = require "resty.redis"
 
 local red = redis:new()
 red:set_timeout(1000)
 local ok, err = red:connect("127.0.0.1", 6379)
 
-if not ok then
-    ngx.log(ngx.ERR, "failed to connect redis: ", err)
-else
-    ngx.log(ngx.INFO, "connection ok:", ok) 
-end
-
-connections_number_list = {}
-
 -- [[ Node host list, you can put it all in your redis cache ]]--
-hosts_list = red:lrange("NODE_HOST_LIST",0, -1) or {}
-ROOM_SIZE = red:get("NODE_ROOM_SIZE") or 9
+origin_host_list = red:lrange("NODE_HOST_LIST",0, -1) or {}
+ROOM_SIZE = tonumber(red:get("NODE_ROOM_SIZE") or '9')
+host_list = {}
 
 -- [[ Core dispatcher ]]--
-for i, host in ipairs(hosts_list)
+for i, host in ipairs(origin_host_list)
 do
     connect_num = tonumber(red:get(host))
-    table.insert(connections_number_list, {host=host, num=connect_num})
+    table.insert(host_list, {host=host, num=connect_num})
 end
 
-table.sort(connections_number_list, function(a, b) return a.num < b.num end)
+table.sort(host_list, function(a, b) return a.num < b.num end)
 the_host = ''
 
 for v in pairs(host_list) do
     if host_list[v].num % ROOM_SIZE ~= 0 then
         the_host = host_list[v].host
-    end
+    end 
 end
 
 if the_host == '' then
-    for v in pairs(host_list)
-    do
-        -- reset the all node used mark to 0
-        -- set redis on production 
-        red:set(host_list[v].host, 0)
-    end
-    -- [[ Rand pick up a node ]] --
+    -- [[ Rand pick up a node , weight sort ~~]] --
     the_host = host_list[math.random(#host_list)].host
 end
 
