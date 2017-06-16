1c96fcd3d6e3b6a7ac2a6698c10efcbe172eaab2
landpack <ada.frank.ak@gmail.com>
2017-06-16 19:25:52 +0800

    [test] msg

--- tests/test_msg.py
+++ tests/test_msg.py
@@ -7,14 +7,22 @@ from kengine.msg import MessageManager, BaseDispatcher
 
 class MyDispatcher(BaseDispatcher):
 
-    def get_matches(self, handler, message):
+    def get_matches(self, handler, ws,  message):
         pass
 
-    def chat(self, handler,  message):
+    def chat(self, handler, ws, message):
         return 'chat_ok'
 
-    def recovery(self, handler, message):
+    def recovery(self, handler, ws, message):
         pass
+
+
+class FakeHandlerManager(object):
+    pass
+
+
+class FakeWebsocketHandler(object):
+    pass
 
 
 class TestMessageManager(unittest.TestCase):
@@ -23,13 +31,16 @@ class TestMessageManager(unittest.TestCase):
         pass
 
     def test_01_message_manage(self):
+        fake_handler_manager = FakeHandlerManager()
+        fake_websocket_handler = FakeWebsocketHandler()
         my_dispatcher = MyDispatcher()
         message_manager = MessageManager(my_dispatcher)
         d1 = {
             "method": "chat",
             "biz_content": "hello world"
         }
-        resp = message_manager.rpc(None, d1)
+        resp = message_manager.rpc(
+            fake_handler_manager, fake_websocket_handler, d1)
         self.assertEqual(resp, 'chat_ok')
 
 
