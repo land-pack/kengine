430e54b09bd61ea6241902c4cf55d86f3f40f4ef
service <frank@linux.com>
2017-06-19 00:13:33 -0400

    [fixed] messageid --> str | if no data , do no response the websocket

--- kengine/handler.py
+++ kengine/handler.py
@@ -130,7 +130,7 @@ class HandlerManager(object):
         else:
             try:
                 data = ujson.loads(message)
-                response = cls.message_manager.rpc(cls, handler, data) or '{}'
+                response = cls.message_manager.rpc(cls, handler, data) or {}
                 data = ujson.dumps(response)
             except Exception as ex:
                 print(traceback.format_exc())
